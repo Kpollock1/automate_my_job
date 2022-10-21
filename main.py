5dac95285b9e58b47ce8b807d1a2cc725423623d
Kpollock1 <53992987+Kpollock1@users.noreply.github.com>
2022-10-21 17:59:23 +0300

    new project

--- main.py
+++ main.py
@@ -7,24 +7,33 @@ import pyautogui as pg, time, os, logging, sys, random, copy, subprocess, static
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
-applied_password = 'Brooklyn132!'
-# Start applied app
-pg.press('win')
-pg.typewrite('applied')
-pg.press('enter')
-logging.debug('Started applied')
-time.sleep(9)
+# applied_password = 'Brooklyn132!'
+# # Start applied app
+# pg.press('win')
+# pg.typewrite('applied')
+# pg.press('enter')
+# logging.debug('Started applied')
+# time.sleep(9)
+#
+#
+# # Type name into applied login
+# pg.typewrite(f'{applied_password}')
+# pg.press('enter')
+# logging.debug('Entered password')
+#
+# time.sleep(17)
+# pg.click(1133, 612,)
+# logging.debug('Clicked continue')
 
+# Open Edge and snap left
+subprocess.call(["C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"])
 
-# Type name into applied login
-pg.typewrite(f'{applied_password}')
-pg.press('enter')
-logging.debug('Entered password')
+time.sleep(.25)
+pg.hotkey('win', 'left')
 
-time.sleep(17)
-pg.click(1133, 612,)
-logging.debug('Clicked continue')
+# Open Outlook and snap left
+subprocess.call(["C:\Program Files\Microsoft Office\\root\Office16\OUTLOOK.EXE"])
 
-time.sleep(9)
+time.sleep(2)
+pg.hotkey('win', 'left')
 
-# subprocess.call(["C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"])
