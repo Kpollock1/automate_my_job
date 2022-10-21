dcd578a89f5d4307da05057d6585c9a90086cd7d
Kpollock1 <53992987+Kpollock1@users.noreply.github.com>
2022-10-21 16:07:14 +0300

    new project

---
+++ main.py
@@ -0,0 +1,30 @@
+"""Automate my job Bot
+
+A program to automate work at AMFIG
+"""
+
+import pyautogui as pg, time, os, logging, sys, random, copy, subprocess
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
+applied_password = 'Brooklyn132!'
+# Start applied app
+pg.press('win')
+pg.typewrite('applied')
+pg.press('enter')
+logging.debug('Started applied')
+time.sleep(9)
+
+
+# Type name into applied login
+pg.typewrite(f'{applied_password}')
+pg.press('enter')
+logging.debug('Entered password')
+
+time.sleep(17)
+pg.click(1133, 612,)
+logging.debug('Clicked continue')
+
+time.sleep(9)
+
+# subprocess.call(["C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"])
